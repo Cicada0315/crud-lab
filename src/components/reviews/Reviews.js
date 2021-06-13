f05807d8be7d2f558d3d24e7a171fd6f3fa8d2f0
jean <jin.ahn.1027@gmail.com>
2021-06-13 15:17:42 -0400

    commit tree 1f78c6fffbfdb75256a62cf5499aa25524fa4f4b

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,12 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const relevantReviews=this.props.reviews.filter(review=> review.restaurantId===this.props.restaurantId)
+    const reviews=relevantReviews.map(review=> <Review review={review} deleteReview={this.props.deleteReview}/>)
     return (
       <ul>
         Reviews
+        {reviews}
       </ul>
     );
   }
